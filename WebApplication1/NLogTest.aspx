c947ef4fe6773901c4a01e9538c0ea522d09ca36
chanshin666 <763225207@qq.com>
2019-04-08 17:27:40 +0800

    签入Nlog写日志测试

---
+++ WebApplication1/NLogTest.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NLogTest.aspx.cs" Inherits="WebApplication1.NLogTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="btnWriteLog" runat="server" Text="写日志" OnClick="btnWriteLog_Click" />
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/NLogTest.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class NLogTest : System.Web.UI.Page
+    {
+        public static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnWriteLog_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("=========================================================\r\n");
+            sb.Append(string.Format("订单{0}付款成功，开始检查拼团状态\r\n", "20190408170122888"));
+            sb.Append("=========================================================\r\n");
+            logger.Debug(sb.ToString());
+            Response.Write("写入日志成功");
+        }
+    }
+}
